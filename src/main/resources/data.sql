3371c823feabb2980a35194640b6367e637c1a24
iamrkgolu <iamrkgolu@gmail.com>
2022-05-26 12:28:45 +0530

    First commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into users (id, username, password, role) values (1,'Jhon','Jhon@1234','Admin');
+insert into users (id, username, password, role) values (2,'David','David@1234','Admin');
+insert into users (id, username, password, role) values (3,'Rahul','Rahul@1234','Admin');
